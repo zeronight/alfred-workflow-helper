aa9879280f35d886ad7b232f75794f0a7d6b9370
zero <zeroxiaobai@gmail.com>
2015-01-16 19:48:52 +0800

    alpha version

---
+++ index.js
@@ -0,0 +1,115 @@
+var builder = require('xmlbuilder');
+var root = builder.create('items');
+
+var AlfredNode = function (item) {
+    this.item = item;
+    this.xmlObj = {};
+
+    this.buildAttributes();
+    this.buildSubItems();
+
+    return this.xmlObj;
+}
+
+AlfredNode.prototype.buildAttributes = function () {
+    var me = this;
+    var attributes = ["uid", "arg", "valid", "autocomplete", "type"];
+
+    attributes.forEach(function (attr) {
+        if (!me.item[attr]) return;
+
+        me.xmlObj['@' + attr] = me.item[attr];
+    });
+}
+
+AlfredNode.prototype.buildSubItems = function () {
+    var me = this;
+    var subItems = ["title", "subtitle", "icon"];
+
+    subItems.forEach(function (subItem) {
+        if (!me.item[subItem]) return;
+
+        me.xmlObj[subItem] = me.item[subItem];
+    });
+
+    me.buildSubtitleItems();
+    me.buildIconItems();
+    me.buildTextItems();
+}
+
+AlfredNode.prototype.buildSubtitleItems = function () {
+    var me = this;
+    var mods = ["shift", "fn", "ctrl", "alt", "cmd"];
+
+    me.xmlObj["#list_shift"] = [];
+    mods.forEach(function (mod) {
+        var value = me.item["subtitle_" + mod]
+
+        if(!value) return;
+
+        me.xmlObj["#list_shift"].push({
+            subtitle: {"#text": value, "@mod": mod}
+        })
+    });
+}
+
+AlfredNode.prototype.buildIconItems = function () {
+    var me = this;
+    var types = ["fileicon", "filetype"];
+
+    me.xmlObj["#list_icon"] = [];
+    types.forEach(function (type) {
+        var value = me.item["icon_" + type]
+
+        if(!value) return;
+
+        me.xmlObj["#list_icon"].push({
+            icon: {"#text": value, "@type": type}
+        })
+    });
+}
+
+AlfredNode.prototype.buildTextItems = function () {
+    var me = this;
+    var types = ["copy", "largetype"];
+
+    me.xmlObj["#list_text"] = [];
+    types.forEach(function (type) {
+        var value = me.item["text_" + type]
+
+        if(!value) return;
+
+        me.xmlObj["#list_text"].push({
+            text: {"#text": value, "@type": type}
+        })
+    });
+}
+
+module.exports = {
+    buildErrorInfo: function (site, message) {
+        this.buildItem({
+            title: site,
+            subtitle: message
+        });
+    },
+
+
+    buildItem: function (item) {
+        var node = new AlfredNode(item);
+
+        root.ele(node);
+    },
+
+    buildItems: function (items) {
+        if(!Array.isArray(items)) {
+            this.buildErrorInfo("buildItems", "Arrguments error: require an array argument.");
+            return;
+        }
+
+        items.forEach(buildItem);
+    },
+
+    end: function () {
+        return root.end({pretty: true});
+    }
+};
---
+++ test/test.js
@@ -0,0 +1,56 @@
+var Alfred = require("../index.js");
+
+var items = [];
+
+items.push({
+    uid: "desktop",
+    arg: "~/Desktop",
+    valid: "YES",
+    autocomplete: "Desktop",
+    type: "file",
+    title: "Desktop",
+    subtitle: "~/Desktop",
+    icon: "~/Desktop"
+});
+
+items.push({
+    uid: "flick",
+    valid: "no",
+    autocomplete: "flickr",
+    title: "Flickr",
+    icon: "flickr.png"
+});
+
+items.push({
+    uid: "image",
+    autocomplete: "My holiday photo",
+    type: "file",
+    title: "My holiday photo",
+    subtitle: "~/Pictures/my holiday photo.jpg",
+    icon: "public.jpeg"
+});
+
+items.push({
+    uid: "home",
+    arg: "~/",
+    valid: "YES",
+    autocomplete: "YES",
+    type: "file",
+    title: "Home Folder",
+    icon_fileicon: "~/",
+    subtitle: "Home Folder ~/",
+    subtitle_shift: "Subtext when shift is pressed",
+    subtitle_fn: "Subtext when fn is pressed",
+    subtitle_ctrl: "Subtext when ctrl is pressed",
+    subtitle_alt: "Subtext when alt is pressed",
+    subtitle_cmd: "Subtext when cmd is pressed",
+    text_copy: "Text when copying",
+    text_largetype: "Text fo LargeType"
+});
+
+Alfred.buildItem(items[items.length - 1]);
+
+var root = Alfred.end();
+
+console.log(root);
+
